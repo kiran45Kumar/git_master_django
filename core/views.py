871da0f39edca4bff7c30d3d9484696c789dd778
kiran45kumar <kirankiccha2003@gmail.com>
2025-07-20 18:19:27 +0530

    Updated homepage message

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to GitMaster Django Project 🚀")
+    return HttpResponse("Updated: Git is awesome! 😎")
