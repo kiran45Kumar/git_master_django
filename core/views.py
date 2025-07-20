d63404484d0ce12bee888abeaa89c46dd3dce6bd
kiran45kumar <kirankiccha2003@gmail.com>
2025-07-20 18:39:30 +0530

    Updated urls.py and views.py

--- core/views.py
+++ core/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    a = 'Kiran'
+def home(request, a):
     return HttpResponse(f"Git is awesome! {a} 😎")
 
